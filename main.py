1abf285bd24f5619cd1429a10770321f43a0af1f
DamnXitri <nico.o.stegmaier@gmail.com>
2022-06-15 20:20:06 -0400

    [update] added --placemapper and --placereducer

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 import argparse
-from src.mappers import CountMapper
-from src.reducers import CountReducer
+from src.mappers import CountMapper, UserMapper
+from src.reducers import CountReducer, UserReducer
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--mapper", action="store_true")
     parser.add_argument("--reducer", action="store_true")
+    parser.add_argument("--placemapper", action="store_true")
+    parser.add_argument("--placereducer", action="store_true")
 
     args = parser.parse_args()
 
     if not (bool(args.mapper) ^ bool(args.reducer)):
-        print("Please specify either --mapper or --reducer")
-        return
+        if not (bool(args.placemapper) ^ bool(args.placereducer)):
+            print("Please specify either --mapper or --reducer")
+            return
 
     if args.mapper:
         mapper = CountMapper()
@@ -21,6 +24,14 @@ def main():
 
     if args.reducer:
         reducer = CountReducer()
+        reducer.run()
+        
+    if args.placemapper:
+        mapper = UserMapper()
+        mapper.run()
+
+    if args.placereducer:
+        reducer = UserReducer()
         reducer.run()
 
 
